659eb6c7c8643d60ea4c2d889d16ca70d4193195
Craytivo <craytivo@gmail.com>
2026-09-07 01:03:44 -0600

    Add sports league hierarchy

---
+++ src/sports/leagues.js
@@ -0,0 +1,11 @@
+export const leagues = [
+  { id: 'nfl', name: 'NFL', shortName: 'NFL', baselineOrder: 1 },
+  { id: 'nba', name: 'NBA', shortName: 'NBA', baselineOrder: 2 },
+  { id: 'ncaa-football', name: 'NCAA Football', shortName: 'NCAA', baselineOrder: 3 },
+  { id: 'laliga', name: 'La Liga', shortName: 'La Liga', baselineOrder: 4 },
+  { id: 'epl', name: 'Premier League', shortName: 'EPL', baselineOrder: 5 },
+  { id: 'mlb', name: 'MLB', shortName: 'MLB', baselineOrder: 6 },
+  { id: 'nhl', name: 'NHL', shortName: 'NHL', baselineOrder: 7 },
+  { id: 'ufc', name: 'UFC', shortName: 'UFC', baselineOrder: 8 },
+  { id: 'ucl', name: 'UEFA Champions League', shortName: 'UCL', baselineOrder: 0 },
+];
