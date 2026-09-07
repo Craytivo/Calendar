f40530bfc0bf53142eae39a2aa0e92836b99ca86
Craytivo <craytivo@gmail.com>
2026-09-07 01:50:11 -0600

    Export TheSportsDB adapter

--- src/sports/adapters/index.js
+++ src/sports/adapters/index.js
@@ -11,7 +11,6 @@ export function createSportsAdapter({ provider, fetchGames }) {
 
   return {
     provider,
-
     async getGames(options = {}) {
       const rawGames = await fetchGames(options);
       return normalizeGames(rawGames);
@@ -20,3 +19,4 @@ export function createSportsAdapter({ provider, fetchGames }) {
 }
 
 export { normalizeGame, normalizeGames, normalizeStatus, normalizeTeam, normalizeTeamContext, isNormalizedGame } from './normalizer.js';
+export { THESPORTSDB_LEAGUES, normalizeTheSportsDbEvents, getTheSportsDbLeague } from './thesportsdb.js';
